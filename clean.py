29e542a26cef2b2b6734bfa52d4600d44e1aaff2
3o5xlle <3o5xlle@gmail.com>
2026-09-17 07:12:31 +0300

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,1 +1,11 @@
+import pandas as pd
+
+# جدول بيانات بسيط
+data = {'المنتج': ['شاحن', 'سماعة', None], 'السعر': [85, 120, None]}
+df = pd.DataFrame(data)
+
+# تنظيف الفراغات
+df.fillna(0, inplace=True)
+print("تم التنظيف بنجاح!")
+print(df)
 
